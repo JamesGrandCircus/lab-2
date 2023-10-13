ceeff497026c9fccd931d4180a71c944b145ed45
Jakyle <jjackson360@gmail.com>
2023-10-12 21:41:11 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Random_Dice_Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rng = new Random();
+            int number = rng.Next();
+
+            Console.WriteLine(number);
+        }
+    }
+}
